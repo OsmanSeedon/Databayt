24ef4ac108f093b9c1d784ae2f614610c6b452ba
Databayt <osmanabdout@hotmail.com>
2023-11-10 11:45:29 +0300

    Starting

--- src/app/tool/page.tsx
+++ src/app/tool/page.tsx
@@ -21,7 +21,10 @@ const Tool = () => {
       </div>
       {toolData.map((values) => {
         return (
-          <div className="py-[3PX]">
+        
+          
+          <div className="py-[3PX]"
+          key={values.id}>
             <Tools
               id={values.id}
               title={values.title}
