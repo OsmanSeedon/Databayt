05cc37057d5203ab95f52e69ee03855cc156d050
osman1 <osmanseedon005@gmail.com>
2023-11-15 15:45:49 +0200

    sh

--- src/components/Nav/Footer.tsx
+++ src/components/Nav/Footer.tsx
@@ -3,9 +3,32 @@ import React from 'react'
 const Footer = () => {
   return (
     
-    <div className="-mx-24">
-      <div className="flex flex-col gap-4 w-full h-[17rem] px-[120px] pt-5 bg-yellow-400 items-center text-right">
+    <div className="flex flex-col bg-yellow-400  text-right  -mx-24">
+      <div className='flex flex-raw'>
+      <div className="flex flex-col gap-2 w-full justify justify-start 
+      pl-28 pt-3 place-items-start ">
+        <div className='text-lg font-bold'>English</div>
+        <div className='text-lg font-bold pb-3'>Light</div>
+        <div className='font-semibold'>Sudan</div>
+        <div className='pb-3'>Khartoum</div>
+        <div className='font-bold'><p className="underline underline-offset-8">Databayt</p></div>
+        <div className=''>Roadmap</div>
+        <div className=''>Skills Set</div>
+        <div className=''>Documentation</div>
+        <div className='pb-28'>Contribute</div>
         
+      </div>
+      <div className='flex flex-col gap-2 w-full justify justify-start 
+      pt-3 place-items-start'>
+        <div className='text-lg font-bold'>Head Office</div>
+        <div className=''>Arkwit, Sudan</div>
+        <div className='scroll-pr-20 pt-3pb-2'> ox: 1470</div>
+
+      </div>
+      </div>
+      <div className='flex justify justify-center  w-full '>
+        <div className='text-sm font-semibold pb-2'>Databayt, All rights free</div>
+
       </div>
     </div>
   )
--- src/components/Text/Read.tsx
+++ src/components/Text/Read.tsx
@@ -8,12 +8,13 @@ const Read = () => {
         <h1 className="font-medium text-[18px]"> Databayt داتابيت</h1>
         <p className="font-medium text-[17px]">مشروع تقني حول أتمتة الأعمال</p>
           </div>
-        <p>
+        <p className="text-black/100">
           داتابيت معنية بتقديم أدوات تساعد على أتمتة الأعمال، وتسعى داتابيت
           لتكون جسرا يوصل بالأدوات المتاحة بالفعل في السوق العالمي،
         </p>
+        <p className="text-black/50">المجانية منها والمدفوعة الثمن, وتسعى كذلك لبناء أدوات من الصفر</p>
         
-        <button className="hover:text-black text-lg">أقرا المزيد</button>
+        <button className="hover:text-blue-600 text-lg">أقرا المزيد</button>
         
       </div>
     
